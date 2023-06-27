1b82ff5d50a1256f23ae8f92cf927db626cff44e
Arihantjain04 <mail.jainarihant@gmail.com>
2023-06-27 22:20:58 +0530

    typing intro animation

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
-let name = document.getElementById('exampleFormControlInput1').innerHTML
-let email = document.getElementById('exampleFormControlInput2')
-let message = document.getElementById('exampleFormControlTextarea1')
-
-let snackbar = document.getElementById('snackbar')
-
-function showSnackbar(){
-    if(name.length > 0){
-        snackbar.classList.add('displayYes')
-    }
+var i = 0
+var txt = "Driven by curiosity and a hunger for knowledge, I'm Passionate about turning ideas into reality through clean and efficient code and Constantly learning and evolving to stay ahead in the ever-changing tech landscape. "
+var speed = 25
+function type(){
+  document.getElementById("type").innerHTML += txt.charAt(i)
+  i++
+  setTimeout(type, speed)
 }
+type();
+
+window.HTMLElement.prototype.scrollIntoView = function() {};
+
+const heroSection = document.querySelector('.hero-div')
+const scrollElement = document.getElementById('scrolltop')
+
+const scrollTop = () => {
+    heroSection.scrollIntoView({behavior: "smooth"})
+}
+
+scrollElement.addEventListener("click", scrollTop)
